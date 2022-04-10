25aeb270c41508a7e3c8e226a584e3f38d65d4c0
Satyadip Singha <satyadipsingha007@gmail.com>
2022-04-10 17:01:51 +0530

    App updates

--- WeatherInformation/Utils/Constants.swift
+++ WeatherInformation/Utils/Constants.swift
@@ -2,25 +2,15 @@
 //  Constants.swift
 //  WeatherInformation
 //
-//  Created by Satyadip Singha on 3/3/21.
-//  Copyright © 2021 Satyadip Singha. All rights reserved.
+//  Created by Satyadip Singha on 08/04/22.
+//  Copyright © 2022 Satyadip Singha. All rights reserved.
 //
 
 import Foundation
 
 struct Constants {
-    
-    struct Urls {
-        
-        static func urlForWeatherByCity(city: String) -> URL {
-            
-            // get the default settings for temperature
-            let userDefaults = UserDefaults.standard
-            let unit = (userDefaults.value(forKey: "unit") as? String) ?? "imperial"
-            
-            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ef0fd9866ca027e0dca474cee84c53be&units=\(unit)")!
-        }
-        
+    static let appID = "ef0fd9866ca027e0dca474cee84c53be"
+    static let unit = {
+        return (UserDefaults.standard.value(forKey: "unit") as? String) ?? "imperial"
     }
-    
 }
